b3313c0e466e94b1a9e5b91742bf7d3cb085ff50
Matthias Bernouy <matt.bernouy@orange.fr>
2024-12-12 09:27:03 +0100

    option controller

--- app/Controllers/OptionController.php
+++ app/Controllers/OptionController.php
@@ -6,6 +6,13 @@ class OptionController extends BaseController
 {
     public function index(): string
     {
-        return '';
+        $response = service('response');
+
+        $response->setHeader('Access-Control-Allow-Origin', getenv("FRONT_URL"))
+            ->setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+            ->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->setHeader('Access-Control-Allow-Credentials', 'true');
+
+        return $response->setStatusCode(200);
     }
 }
